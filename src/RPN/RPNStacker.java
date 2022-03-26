6d621d4dfad8dd56e7c151558d14b4c4f087dbff
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2022-03-26 05:43:21 -0300

    Atualização: pegando os valores de um arquivo

--- src/RPN/RPNStacker.java
+++ src/RPN/RPNStacker.java
@@ -1,32 +1,38 @@
+package RPN;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class RPNStacker {
-    public static void main(String[] args) {
-        StringTokenizer st = new StringTokenizer("4 8 + 3 *");
-        Stack<String> stack = new Stack<String>();
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("src/Calc1.stk");
+        String content = new String(Files.readAllBytes(path));
+        StringTokenizer st = new StringTokenizer(content, "\r\n");
+        Stack<String> stack = new Stack<>();
         while (st.hasMoreTokens()) {
             String token = st.nextToken();
-            if (token.equals("+")) {
+            if ("+".equals(token)) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
-                 stack.push(String.valueOf(a + b));
-
-            }else if (token.equals("*")) {
+                stack.push(String.valueOf(a + b));
+            } else if ("*".equals(token)) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
-                stack.push(String.valueOf(a * b));
-
-            }else if (token.equals("-")) {
+                int calc = a * b;
+                stack.push(String.valueOf(calc));
+            } else if ("-".equals(token)) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
                 stack.push(String.valueOf(b - a));
-
-            }else if (token.equals("/")) {
+            } else if ("/".equals(token)) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
                 stack.push(String.valueOf(b / a));
-            }else {
+            } else {
                 stack.push(token);
             }
 
