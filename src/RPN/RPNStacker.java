5b81c7ac227140a219faad3707f2a1c55d188053
Vinícius Felipe <56048036+Viniciusfelp@users.noreply.github.com>
2022-03-26 08:18:48 -0300

    Atualização: pegando os valores de um arquivo

--- src/RPN/RPNStacker.java
+++ src/RPN/RPNStacker.java
@@ -3,32 +3,30 @@ package RPN;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class RPNStacker {
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("src/Calc1.stk");
+        Path path = Path.of("src/Calc1.stk");
         String content = new String(Files.readAllBytes(path));
         StringTokenizer st = new StringTokenizer(content, "\r\n");
         Stack<String> stack = new Stack<>();
         while (st.hasMoreTokens()) {
             String token = st.nextToken();
-            if ("+".equals(token)) {
+            if (token.equals("+")) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
                 stack.push(String.valueOf(a + b));
-            } else if ("*".equals(token)) {
+            } else if (token.equals("*")) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
-                int calc = a * b;
-                stack.push(String.valueOf(calc));
-            } else if ("-".equals(token)) {
+                stack.push(String.valueOf(a*b));
+            } else if (token.equals("-")) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
                 stack.push(String.valueOf(b - a));
-            } else if ("/".equals(token)) {
+            } else if (token.equals("/")) {
                 int a = Integer.parseInt(stack.pop());
                 int b = Integer.parseInt(stack.pop());
                 stack.push(String.valueOf(b / a));
